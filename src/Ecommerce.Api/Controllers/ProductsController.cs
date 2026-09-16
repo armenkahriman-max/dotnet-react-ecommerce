d9f9663d8e0b9b6c009e4785cd4cf9ece928828b
armenkahriman-max <armenkahriman93@gmail.com>
2026-09-16 10:46:37 +0200

    Add product  GET and POST Endpoints

---
+++ src/Ecommerce.Api/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+using Ecommerce.Api.Data;
+using Ecommerce.Api.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(AppDbContext dbContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetAll()
+    {
+        var products = await dbContext.Products
+        .OrderBy(product => product.Name)
+        .ToListAsync();
+
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetById(int id)
+    {
+        var product = await dbContext.Products.FindAsync(id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Product product)
+    {
+        dbContext.Products.Add(product);
+        await dbContext.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+    }
+}
